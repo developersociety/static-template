8f4ca57b4daab3627cf81af9c53105fe96f4e43c
Alex Tomkins <tomkins@darkzone.net>
2016-06-13 12:25:06 +0100

    Initial commit

---
+++ README.rst
@@ -0,0 +1,20 @@
+=====================
+Blanc Static Template
+=====================
+
+Installation
+------------
+
+Install cookiecutter_:
+
+.. _cookiecutter: https://github.com/audreyr/cookiecutter
+
+.. code-block:: console
+
+    $ pip install --user cookiecutter
+
+Create a site using this template:
+
+.. code-block:: console
+
+    $ cookiecutter gh:blancltd/static-template
---
+++ {{cookiecutter.project_slug}}/fabfile.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+
+from fabric.api import cd, env, parallel, roles, run, task
+from fabric.contrib.files import exists
+
+# Changable settings
+env.roledefs = {
+    'web': [
+        'staticsites@cornwall.blanctools.com',
+        'staticsites@devon.blanctools.com',
+    ],
+}
+
+env.home = env.get('home', '/var/www/staticsites/{{ cookiecutter.project_slug }}')
+env.repo = env.get('repo', '{{ cookiecutter.project_slug }}')
+
+# Avoid tweaking these
+env.use_ssh_config = True
+GIT_REMOTE = 'git@github.com:blancltd/{env.repo}.git'
+
+
+@task
+@roles('web')
+@parallel
+def deploy(branch=None):
+    """
+    Deploy to remote server.
+
+    fab deploy
+    fab deploy:branch
+    """
+    if not exists(env.home):
+        run('mkdir -p {}'.format(env.home))
+
+    with cd(env.home):
+        if not exists('.git'):
+            git_repo = GIT_REMOTE.format(env=env)
+            run('git clone --quiet --recursive {} .'.format(git_repo))
+
+        if branch is not None:
+            run('git fetch')
+            run('git checkout {}'.format(branch))
+
+        run('git pull --quiet')
